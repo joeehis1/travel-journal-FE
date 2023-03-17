a2358806942a61117c1d6a7f2d45b79dac5c71cb
joeehis1 <joeehis1@gmail.com>
2023-03-17 00:49:52 -0400

    Added Footer Section. Updated Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Header } from "./components/Header"
 import { Main } from "./components/Main"
 import {data} from './data'
-
+import { Footer } from "./components/Footer"
 
 function App(){
     return (
         <>
             <Header />
             <Main locations={data}/>
+            <Footer />
         </>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer(){
     return(
         <footer>
-            <p style={fontSize ='0.6rem'}>Built by Joseph</p>
+            <p style={{fontSize :'0.6rem'}}>Built by Joseph</p>
         </footer>
     )
 }
